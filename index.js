f2a39840a7b82f9f0bf5d94d76b3864e983e08bb
rohithmale <rm982@njit.edu>
2023-02-13 14:09:12 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function background() {
+    document.querySelectorAll('.hideable').forEach(el => el.classList.toggle('hidden'));
+  }
+
+function foreground() {
+    document.querySelectorAll('.hideable').forEach(el => el.classList.toggle('hidden'));
+  }
