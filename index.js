19441ef233de80c99c8bcc954a71ba0992b3cdac
rohithmale <rm982@njit.edu>
2023-02-14 21:29:02 -0500

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function background() {
     document.querySelectorAll('.hideable').forEach(el => el.classList.toggle('hidden'));
   }
@@ -5,3 +6,46 @@ function background() {
 function foreground() {
     document.querySelectorAll('.hideable').forEach(el => el.classList.toggle('hidden'));
   }
+
+  document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const subject = document.getElementById("sub").value;
+    const content = document.getElementById("content").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    if (!subject) {
+      alert("Subject is required");
+      return;
+    }
+
+    if (!content) {
+      alert("Message is required");
+      return;
+    }
+
+    if (!name) {
+      alert("Name is required");
+      return;
+    }
+
+    if (!email) {
+      alert("Email is required");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      alert("Invalid email address");
+      return;
+    }
+
+    alert("Form submitted successfully");
+  });
+
+
+
+// rohith male
+// 2-13-23
+// ucid: rm982
+
